19809284f3196289f8d3407af9d83964fc1bcfa5
Sam Stone <sjstone1987@gmail.com>
2016-10-08 16:28:34 -0400

    add positions loop

--- get_urls.py
+++ get_urls.py
@@ -2,32 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-POSITIONS = ['P', 'C', 'SS', 'OF', '1B', '2B', '3B', 'INF']
+# Ignore INF (Infielder); only 3 in 1990 and none after
+POSITIONS = ['P', 'C', 'SS', 'OF', '1B', '2B', '3B']
 
-position = 'P'
-year = 1990
-QUERY = '?query_type=pos_year&pos={}&year_ID={}&draft_type=junreg&'.format(position, year)
-URL = 'http://www.baseball-reference.com/draft/' + QUERY
+def write_players_by_year(year):
+    for position in POSITIONS:
+        write_players_by_position(year, position)
 
-# print URL
+def write_players_by_position(year, position):
+    query = ('?query_type=pos_year&pos='
+             '{}&year_ID={}&draft_type=junreg&'.format(position, year))
+    url = 'http://www.baseball-reference.com/draft/' + query
 
-r = requests.get(URL)
-if not r.ok:
-    raise RuntimeError('Error connecting to %s' % r.url)
+    # print url
 
-bs = BeautifulSoup(r.text, 'html.parser')
+    r = requests.get(url)
+    if not r.ok:
+        raise RuntimeError('Error connecting to %s' % r.url)
 
-table = bs.find(id='draft_stats')
+    bs = BeautifulSoup(r.text, 'html.parser')
 
-players = []
+    table = bs.find(id='draft_stats')
 
-for a in table.find_all('a'):
-    link = a.get('href')
-    if link[0:23] == '/register/player.cgi?id':
-        players.append([year, position, link])
+    players = []
 
-# print len(links)
-# print links
+    for a in table.find_all('a'):
+        link = a.get('href')
+        if link[0:23] == '/register/player.cgi?id':
+            players.append([year, position, link])
 
-df = pd.DataFrame(players, columns=['Year', 'Position', 'Link'])
-df.to_csv('draftees.csv', header=True)
+    # print len(links)
+    # print links
+
+    df = pd.DataFrame(players, columns=['Year', 'Position', 'Link'])
+    df.to_csv('draftees.csv', mode='a', header=True)
+
+for year in range(1990, 1991):
+    write_players_by_year(year)
--- scrape.py
+++ scrape.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 
-URL = 'http://www.baseball-reference.com/register/player.cgi?id='
+URL = 'http://www.baseball-reference.com'
 
-def write_player_data(player_id, start_time):
+DATA_TYPES = ['standard_pitching', 'standard_fielding', 'standard_batting']
+
+def write_player_data(player_id):
     # Append player data to CSV.
     r = requests.get(URL + player_id)
     if not r.ok:
@@ -13,18 +15,20 @@ def write_player_data(player_id, start_time):
                            "Make sure to add retries to code!""" % r.url)
 
     bs = BeautifulSoup(r.text, 'html.parser')
-    table = bs.find(id='standard_pitching')
+    for data_type in DATA_TYPES:
+        table = bs.find(id=data_type)
 
-    header, data = extract_table(table, player_id)
-
-    df = pd.DataFrame(data, columns=header)
-    # df.to_csv('baseball.csv')
-    df.to_csv('baseball.csv', mode='a', header=False)
+        # TODO: If no table, player probably didn't sign?
+        # Ask Steven how to handle this situation
+        if table:
+            data = extract_table(table, player_id)
+            df = pd.DataFrame(data)
+            df.to_csv(data_type + '.csv', mode='a', header=False)
 
 def extract_table(table, player_id):
     # Return header (th) and data (td) from an html table.
-    header = [th.text for th in table.thead.find_all('th')]
-    header.insert(0, 'player_id')
+    # header = [th.text for th in table.thead.find_all('th')]
+    # header.insert(0, 'player_id')
     data = []
     for tr in table.tbody.find_all('tr'):
         record = [player_id]
@@ -32,17 +36,17 @@ def extract_table(table, player_id):
         for td in tds:
             record.append(td.text.encode('utf-8').strip())
         data.append(record)
-    return header, data
+    # return header, data
+    return data
+
 
 start_time = datetime.datetime.now()
 
-player_ids = [
-    'osborn002don',
-    'vanpop001tod',
-    'smith-015dan'
-]
-for player_id in player_ids:
-    write_player_data(player_id, start_time)
+draftees = pd.read_csv('draftees.csv').values.tolist()
+
+for draftee in draftees[5:10]:
+    player_id = draftee[3]
+    write_player_data(player_id)
     end_time = datetime.datetime.now()
     delta = end_time - start_time
     print "Wrote {} in {}.{} seconds".format(player_id,
